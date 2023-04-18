ef496762dfe9fcfb86237980295ff10a83e96b7c
zhangwenbo <1953954864@qq.com>
2023-04-18 11:11:07 +0800

    created docs

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,8 +3,13 @@
 import './styles/index.css';
 import 'element-ui/lib/theme-chalk/index.css';
 import ElementUI from 'element-ui/lib/index';
-import zElementUI from '../../packages/index';
 export default ({ Vue }) => {
   Vue.use(ElementUI, { size: 'mini' });
-  Vue.use(zElementUI);
+  Vue.mixin({
+    mounted() {
+      import('../../packages/index').then(function (m) {
+        Vue.use(m);
+      });
+    }
+  });
 };
